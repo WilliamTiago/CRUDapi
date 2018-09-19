2d948857123eb2dcaa8415ec714d66d3993fd887
William Tiago Velho da Silva Goebel <tiagowtvs@hotmail.com>
2018-09-18 21:11:53 -0300

    nova release

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+  
+    <title>Exemplo jQuery</title>
+</head>
+<body>
+<div class="container">
+    <!-- Content here -->
+    <nav class="navbar navbar-dark bg-dark">
+    <!-- Navbar content -->
+    </nav>
+    <input type="button" value="buscar" id="buscar">
+    <div id="resultado"></div>
+    <input hidden type="text" id="alteraNome">
+    <input hidden type="button" value="Confirmar" class="confirmar"></br>
+    <input type="text" id="nome">
+    <input type="button" value="gravar" id="gravar">
+</div>
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+  $("#buscar").click(function() {
+    $.getJSON("http://serene-chamber-53332.herokuapp.com/api/pessoa", function(data, status) {
+      $("#resultado").html("");
+      var items = [];
+      $.each(data, function(key, val) {
+        console.log(key);
+        items.push("<tr><td>" + val.nome + "</td><td><button class=\"excluir\" id=\""+ val.id + "\">Excluir</button></td><td><button  class=\"alterar\" name=\""+val.nome+"\"id=\""+ val.id + "\">Alterar</button></td></tr>");
+      });
+
+      $("<table/>", {
+        class: "vermelho",
+        border: "1",
+        html: items
+      }).appendTo("#resultado");
+
+      //Excluir
+      $(".excluir").click(function() {
+        var id = $(this).attr('id');
+        $.ajax({           
+          type: "DELETE",
+          url: "http://localhost:41071/pessoa/"+id,
+          data: "",
+          success: function(data) {
+            //alert("Registro " + id + " excluido com sucesso!");
+          },
+          contentType: "application/json",
+          dataType: "json"
+        }).then(res => {
+          $("#buscar").click();
+        });
+      });  
+      
+      //Alterar
+      $(".alterar").click(function() {
+        var id = $(this).attr('id');
+        console.log(id);
+        var nome = $(this).attr('name');
+        $("#alteraNome").show("slow");
+        $("#alteraNome").val(nome);
+        $(".confirmar").attr('id', id);
+        $(".confirmar").show("slow");
+        
+      });
+    });
+  });
+
+  $(".confirmar").click(function() {
+    let sNome = $("#alteraNome").val();
+    var id = $(this).attr('id');
+    $.ajax({           
+      type: "PATCH",
+      url: "http://localhost:41071/pessoa/"+id,
+      data: JSON.stringify ({nome: sNome}),
+      success: function(data) {
+        //alert("Registro " + id + " alteradocom sucesso!");
+      },
+      contentType: "application/json",
+      dataType: "json"
+    }).then(res => {
+      $("#buscar").click();
+      $("#alteraNome").hide("slow");
+      $(".confirmar").hide("slow");
+    });
+  });
+
+  $("#gravar").click(function() {
+    //enviado
+    let sNome = $("#nome").val();    
+    $.ajax({           
+      type: "POST",
+      url: "http://localhost:41071/pessoa",
+      data: JSON.stringify ({nome: sNome}),
+      success: function(data) {
+        //alert("data: " + data);
+      },
+      contentType: "application/json",
+      dataType: "json"
+    }).then(res => {
+      $("#buscar").click();
+    });
+  });
+});
